e9b3c5a48c2df71e997f744f2ab31c1209174103
Joneck <eryk.dziemidzionek@gmail.com>
2024-10-21 23:52:12 +0200

    Change movement script, now it works with AddForce. I need to find a way to make grappling gun work

--- Assets/Game/Scripts/Grappling.cs
+++ Assets/Game/Scripts/Grappling.cs
@@ -9,6 +9,8 @@ public class Grappling : MonoBehaviour
     public GameObject cam;
     Rigidbody rb;
 
+    bool grapple = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +24,18 @@ public class Grappling : MonoBehaviour
 
         if(Input.GetMouseButtonDown(1))
         {
+            grapple = true;
+        }
+    }
 
-
+    void FixedUpdate()
+    {
+        if(grapple)
+        {
+            grapple = false;
             rb.AddForce(cam.transform.forward * 45, ForceMode.Impulse);
             Debug.Log("grappled");
         }
+        
     }
 }
--- Assets/Game/Scripts/Player/PlayerMovement.cs
+++ Assets/Game/Scripts/Player/PlayerMovement.cs
@@ -23,7 +23,9 @@ public class PlayerMovement : MonoBehaviour
 
     public Animator anim;
 
-
+    public float accelerationForce = 10;
+    public float maxMovementSpeed = 50;
+    public float maxSpeed = 7;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -53,14 +55,16 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetTrigger("StandUp");
         }
+
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        vertical = Input.GetAxis("Vertical");
-        horizontal = Input.GetAxis("Horizontal");
+        /*
 
-        moveDirection = orientation.forward * vertical + orientation.right * horizontal;
+        
 
         if(Input.GetKey(KeyCode.LeftShift))
         {
@@ -73,7 +77,31 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 velocity = moveDirection * actualSpeed * Time.fixedDeltaTime;
         velocity.y = rb.velocity.y;
-        rb.velocity = velocity;
+        rb.velocity = velocity;*/
+
+        Movement();
+        SpeedControl();
+    }
+
+    private void Movement()
+    {
+        moveDirection = orientation.forward * vertical + orientation.right * horizontal;
+        print(moveDirection);
+        Vector3 velocity = moveDirection * accelerationForce * Time.fixedDeltaTime * 1600;
+        
+        rb.AddForce(velocity,ForceMode.Force);
+    }
+
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x,0,rb.velocity.z);
+        
+        if(flatVel.magnitude > maxMovementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * maxMovementSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
